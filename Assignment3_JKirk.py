875b47223c114e859658f4d769f8ea6aed35b8f1
Jamey Kirk <jameykirk@gmail.com>
2021-03-01 01:03:21 +0900

    All Setup

--- Assignment3_JKirk.py
+++ Assignment3_JKirk.py
@@ -4,3 +4,10 @@ Title: Assignment3_Turtle2
 Date: 02/28/2021
 Description: use turtle to draw more polygons
 """
+## setup turtle
+import turtle
+s = turtle.getscreen()
+t = turtle.getturtle()
+
+
+turtle.exitonclick()
